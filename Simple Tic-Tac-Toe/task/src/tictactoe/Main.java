6f53a63cfb006f425b2134cfe2f646dfc9caef54
Jhan Frank <JhnFrankz@gmail.com>
2022-06-01 12:01:08 -0500

    Finished Stage 4/5 TicTacToe

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -10,6 +10,57 @@ public class Main {
         String line = scanner.nextLine();
         char[][] x = new char[3][3];
         int aux = 0;
+
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < x[i].length; j++) {
+                x[i][j] = line.charAt(aux);
+                aux++;
+            }
+        }
+
+        printGrid(x);
+
+        int coordX = 0;
+        int coordY = 0;
+        boolean isValidCoord = true;
+
+        do {
+            try {
+                System.out.print("Enter the coordinates: ");
+                coordX = Integer.parseInt(scanner.next());
+                coordY = Integer.parseInt(scanner.next());
+
+                if (!(coordX >= 1 && coordX <= 3) || !(coordY >= 1 && coordY <= 3)) {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                    isValidCoord = false;
+                    continue;
+                }
+
+                if (x[coordX - 1][coordY - 1] != '_') {
+                    System.out.println("This cell is occupied! Choose another one!");
+                    isValidCoord = false;
+                    continue;
+                }
+
+                //If there aren't error, gets here
+                isValidCoord = true;
+            } catch (NumberFormatException e) {
+                System.out.println("You should enter numbers!");
+                isValidCoord = false;
+            }
+
+        } while (!isValidCoord);
+
+        x[coordX - 1][coordY - 1] = 'X';
+
+        printGrid(x);
+
+
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter cells: ");
+        String line = scanner.nextLine();
+        char[][] x = new char[3][3];
+        int aux = 0;
         int conX = 0;
         int conO = 0;
 
@@ -68,7 +119,7 @@ public class Main {
         }
 
         if ((conO - conX >= 2 || conX - conO >= 2) ||
-            existLineX && existLineO) {
+                existLineX && existLineO) {
             System.out.println("Impossible");
         } else if (!existLineO && !existLineX && conX + conO == 9) {
             System.out.println("Draw");
@@ -78,6 +129,18 @@ public class Main {
             System.out.println("O wins");
         } else {
             System.out.println("Game not finished");
+        }*/
+    }
+
+    private static void printGrid(char[][] x) {
+        System.out.println("---------");
+        for (int i = 0; i < x.length; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < x[i].length; j++) {
+                System.out.printf("%s ", x[i][j]);
+            }
+            System.out.println("|");
         }
+        System.out.println("---------");
     }
 }
